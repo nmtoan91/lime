b61b2c4938572e6e0ef42a03257f0a47b009bf72
nmtoan91 <nmtoan91@gmail.com>
2025-03-11 14:37:23 +0900

    save

---
+++ toanstt/analyzing/main.py
@@ -0,0 +1,51 @@
+import numpy as np
+import scipy.stats as stats
+from sklearn.metrics import mean_squared_error, mean_absolute_error
+import pandas as pd
+# 0:  groundtruth label
+# 1: predict label
+# 2: is_correct
+# 3: explaining_label
+# 4: intercept_conflict
+# 5: local_pred_conflict
+# 6: score_conflict
+data = np.load('toanstt/data/data.npy')
+column_names = ['groundtruth_label','predict label','is_correct', 'explaining_label','intercept_conflict','local_pred_conflict','score_conflict']
+
+
+input_attributes = [4,5,6]
+n_class = data.shape[1]
+dfs = []
+
+for l in range(n_class):
+    d = data[:,l,:]
+    dfs.append(pd.DataFrame(d, columns=column_names))
+
+sheet_names = [str(i) for i in range(n_class)]
+with pd.ExcelWriter('toanstt/analyzing/data/data.xlsx') as writer:
+    for df, sheet in zip(dfs, sheet_names):
+        df.to_excel(writer, sheet_name=sheet, index=False)
+
+for l in range(n_class):
+    d = data[:,l,:]
+    y = d[2]
+    for input_attribute in input_attributes:
+        x = d[input_attribute]
+        pearson_corr, pearson_pvalue = stats.pearsonr(x, y)
+        spearman_corr, spearman_pvalue = stats.spearmanr(x, y)
+        mse = mean_squared_error(x, y)
+        rmse = np.sqrt(mse)
+        mae = mean_absolute_error(x, y)
+        
+        print(f"Pearson correlation coefficient: {pearson_corr:.4f} (p-value: {pearson_pvalue:.4f})")
+        print(f"Spearman correlation coefficient: {spearman_corr:.4f} (p-value: {spearman_pvalue:.4f})")
+        print(f"Mean Squared Error (MSE): {mse:.4f}")
+        print(f"Root Mean Squared Error (RMSE): {rmse:.4f}")
+        print(f"Mean Absolute Error (MAE): {mae:.4f}",'\n\n')
+        pass
+    
+pass
+
+
+
+
--- toanstt/main.py
+++ toanstt/main.py
@@ -1,3 +1,4 @@
+from TabularExplainer import TabularExplainer
 import os
 import sys
 import inspect
@@ -21,6 +22,8 @@ import lime.lime_tabular
 import json
 
 from MTools import LoadDataSet, SelectClassifier,ExtractExplnationData,CheckArgs
+from sklearn.metrics import accuracy_score
+from tqdm import tqdm
 
 dirname = os.path.dirname(__file__)
 basename =os.path.basename(__file__)
@@ -44,7 +47,12 @@ def ExecuteExplain(args):
     classifier = SelectClassifier(args.method)
     classifier.fit(X_train, y_train)
 
-    
+    # if True:
+    #     y_predict = classifier.predict(X_test)
+    #     accuracy = accuracy_score(y_predict, y_test)
+    #     correct_indices = np.where(y_predict == y_test)[0]
+    #     wrong_indices = np.where(y_predict != y_test)[0]
+    #     pass
     instance = X_test.iloc[args.index]
     if args.label == None:
         #args.label = classifier.predict([instance])[0]
@@ -94,10 +102,62 @@ def ExecuteExplain(args):
     return exp,fig 
 
 
+def Analyze(args):
+    CheckArgs(args)
+
+    data = LoadDataSet(args.data)
+    X = data.data
+    y = data.target
+    
+    feature_names = data.feature_names
+
+    df = pd.DataFrame(X, columns=feature_names)
+    df['target'] = y
+
+    X_train, X_test, y_train, y_test = train_test_split(
+        df[feature_names], df['target'], test_size=0.2, random_state=42)
+
+    # Train a Random Forest classifier
+    classifier = SelectClassifier(args.method)
+    classifier.fit(X_train, y_train)
+
+    y_predict = classifier.predict(X_test)
+    accuracy = accuracy_score(y_predict, y_test)
+    accuracy_list = list(y_predict == y_test)
+    correct_indices = np.where(y_predict == y_test)[0]
+    wrong_indices = np.where(y_predict != y_test)[0]
+        
+    
+    y_test = y_test.to_numpy()
+    n_class = len(np.unique(y_train))
+    data = np.zeros((len(X_test),n_class,3 +  3 +1))
+    explainer = TabularExplainer(np.array(X_train),feature_names,class_names=feature_names,mode='classification',training_labels=None)
+    for i in tqdm(range(len(X_test))):
+        instance = X_test.iloc[args.index]
+        for l in range(n_class):
+            exp = explainer.explain_instance(
+                data_row=instance,
+                predict_fn=classifier.predict_proba, labels=(l,),
+                alpha = args.alpha, num_features = args.num_features
+            )
+            data[i,l,0] = y_test[i]
+            data[i,l,1] = y_predict[i]
+            data[i,l,2] = accuracy_list[i]
+            data[i,l,3] = l
+            data[i,l,4] =  exp.intercept_conflict[l]
+            data[i,l,5] =  exp.local_pred_conflict[l]
+            data[i,l,6] =  exp.score_conflict[l]
+            pass
+    pass
+    np.save('toanstt/data/data.npy', data)
+
+   
+
+
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(formatter_class=argparse.ArgumentDefaultsHelpFormatter)
     #parser.add_argument("-d", "--data",type=str,  default='covtype', help='data')
-    parser.add_argument("-d", "--data",type=str,  default='iris', help='data')
+    parser.add_argument("-d", "--data",type=str,  default='breast_cancer', help='data')
     parser.add_argument("-i", "--index",type=int,  default=3, help='index')
     parser.add_argument("-l", "--label",type=int,  default=None, help='index')
     parser.add_argument("-m", "--method",type=str,  default='KNeighborsClassifier', help='method')
@@ -106,7 +166,8 @@ if __name__ == '__main__':
     #parser.add_argument("-e", "--explainer",type=str,  default='lime', help='alpha')
     parser.add_argument("-nf", "--num_features",type=int,  default=10, help='num_features')
     args = parser.parse_args()
-    ExecuteExplain(args)
+    #ExecuteExplain(args)
+    Analyze(args)
 
 
 
